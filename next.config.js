f427ccd90a49baae42f09b4a648164fb1edb6248
VictorCiechovicz <victor09ciechovicz@outlook.com>
2023-10-27 23:02:35 -0300

    fix build

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    swcPlugins: [['next-superjson-plugin', {}]]
-  },
   images: {
     domains: ['res.cloudinary.com']
   }
